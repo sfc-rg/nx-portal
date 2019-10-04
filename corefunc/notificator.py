e817520e9afbc44b604f73b0894ea54d49817d02
Shota Shimazu <hornet.live.mf@gmail.com>
2019-10-04 20:23:59 +0900

    revise

---
+++ corefunc/notificator.py
@@ -0,0 +1,21 @@
+
+class SlackNotificator():
+    
+
+
+    def shoot(self, text):
+
+        try:
+            is_slack = request.POST['is_slack']
+
+
+            WEB_HOOK_URL = settings.SLACK_INCOMING_TOKEN
+
+            requests.post(WEB_HOOK_URL, data = json.dumps({
+                'text': text,
+                'username': u'{0} Portal System'.format(settings.APPLICATION_NAME),
+                'icon_emoji': u':smile_cat:',
+                'link_names': 1,
+            }))
+        except:
+            pass
