b0ebfa5769c860541e14a8de03b2c63c6bbf74ca
Shota Shimazu <hornet.live.mf@gmail.com>
2019-10-05 15:18:36 +0900

    enable webpack loader

--- portal/settings.py
+++ portal/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'webpack_loader',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
