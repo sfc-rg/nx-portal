3873d51e1dd8cb6aab11ea647a0e39826df031de
Shota Shimazu <hornet.live.mf@gmail.com>
2020-05-14 00:38:35 +0900

    add db

--- meeting/admin.py
+++ meeting/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import (
+    Meeting, MeetingPresenter, MeetingFilesRegistration
+)
 
-# Register your models here.
+admin.site.register(Meeting, MeetingPresenter, MeetingFilesRegistration)
