5676d4f637ebb988a5e0420736def97c1383b7ed
Shota Shimazu <hornet.live.mf@gmail.com>
2020-05-20 13:47:04 +0900

    add ldap backend app

--- meeting/admin.py
+++ meeting/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import (
-    Meeting, MeetingPresenter, MeetingFilesRegistration
-)
+# from .models import (
+#     Meeting, MeetingPresenter, MeetingFilesRegistration
+# )
 
-admin.site.register(Meeting, MeetingPresenter, MeetingFilesRegistration)
+# admin.site.register(Meeting, MeetingPresenter, MeetingFilesRegistration)
